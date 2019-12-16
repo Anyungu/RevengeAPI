d6ee08420c3620ca94aa3960bb54e96ea2ed99e1
Anyungu <anyungucw@gmail.com>
2019-12-16 10:27:12 +0300

    First One

---
+++ src/main/java/com/anyungu/revenge/revengeAPI/RevengeApiApplication.java
@@ -0,0 +1,13 @@
+package com.anyungu.revenge.revengeAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevengeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevengeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anyungu/revenge/revengeAPI/RevengeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anyungu.revenge.revengeAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevengeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
